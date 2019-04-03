2d7fdcd80e6f7bcef242fea609b95153f15c27b3
natetem <mariejeanne.natete@gmail.com>
2019-04-03 23:07:20 +0200

    first commit

---
+++ src/main/java/ApplicationMain.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ApplicationMain {
+
+    public List<int[]> partition(int [] listIn, int n){
+        System.out.println("**** Number of elements **** : " + n );
+        int sizeOfList = listIn.length;
+        int z = (int) Math.ceil((double) sizeOfList / n);
+        System.out.println("**** Length of list*** : " + z);
+        List<int[]> list = new ArrayList();
+        for (int i = 0; i < z; i++) {
+            int from = i * n;
+            int j = (i + 1) * n;
+            int to = (j < sizeOfList) ? j : sizeOfList;
+            int[] s = Arrays.copyOfRange(listIn, from, to);
+            list.add(s);
+        }
+        list.stream().map(s -> Arrays.toString(s)).forEach(System.out::println);
+        return list;
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/ApplicationMainTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationMainTest {
+
+    private static ApplicationMain api;
+
+    @BeforeAll
+    public static void beforeMain(){
+        api=new ApplicationMain();
+    }
+
+   @BeforeEach
+    public  void beforeTest(){
+        System.out.println(" Before*******************");
+    }
+
+    @AfterEach
+    public  void afterTest(){
+        System.out.println(" After*******************");
+    }
+
+
+    @Test
+    void partition_for_two_length() {
+        int[] list={1,2,3,4,5};
+        int n=2;
+        List<int[]> listOut=api.partition(list,n);
+        int [] excepted={1,2};
+        assertArrayEquals(listOut.get(0),excepted);
+    }
+
+    @Test
+    void partition_for_three_length() {
+        int[] list={1,2,3,4,5};
+        int n=3;
+        List<int[]> listOut=api.partition(list,n);
+        assertEquals(listOut.size(),2);
+    }
+
+    @Test
+    void partition_for_one_length() {
+        int[] list={1,2,3,4,5};
+        int n=1;
+        List<int[]> listOut=api.partition(list,n);
+        assertEquals(listOut.size(),5);
+    }
+}
